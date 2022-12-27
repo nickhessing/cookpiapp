4ca9ccb7d7ad84db708a6cd7a132f403abaacdbd
GohesDigital <70105342+GohesDigital@users.noreply.github.com>
2022-12-27 21:22:50 +0100

    delete unnessacary

--- src/app.py
+++ src/app.py
@@ -275,7 +275,6 @@ external_stylesheets = [
 ]
 
 app = dash.Dash(__name__,external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
-
 server = app.server
 
 app.css.config.serve_locally = True
